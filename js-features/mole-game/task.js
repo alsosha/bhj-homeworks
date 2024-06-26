0684cd0a42e43353a809a56fc13c2546e835f0c5
Alsosha <barsuk2001@gmail.com>
2024-06-26 12:10:41 +0300

    Доработано третье задание первого дз

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,16 +2,26 @@ const killCount = document.getElementById('dead')
 const missCount = document.getElementById('lost')
 
 for (let i = 1; i < 10; i++) {
-    holeId = `hole${i}`;
+    const holeId = `hole${i}`;
     const hole = document.getElementById(holeId);
     hole.onclick = clickChecker;
 }
 
 function clickChecker() {
     holeClass = this.className
-    if (holeClass === 'hole hole_has-mole') {
-        killCount.textContent = Number(killCount.textContent) + 1;
-    } else {
-        missCount.textContent = Number(missCount.textContent) + 1;
+        if (holeClass === 'hole hole_has-mole') {
+            killCount.textContent = Number(killCount.textContent) + 1;
+            if (Number(killCount.textContent) === 10) {
+                window.alert('Победа!');
+                killCount.textContent = 0
+                missCount.textContent = 0
+            }
+        } else {
+            missCount.textContent = Number(missCount.textContent) + 1;
+            if (Number(missCount.textContent) === 5) {
+                window.alert('Вы проиграли!');
+                killCount.textContent = 0
+                missCount.textContent = 0
+            }
+        }
     }
-}
