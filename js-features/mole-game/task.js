21afc150a469afe2a3680d8d151250de7bbf942d
Alsosha <barsuk2001@gmail.com>
2024-06-30 15:25:08 +0300

    Выполнено третье домашнее задание

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -9,19 +9,19 @@ for (let i = 1; i < 10; i++) {
 
 function clickChecker() {
     holeClass = this.className
-        if (holeClass === 'hole hole_has-mole') {
-            killCount.textContent = Number(killCount.textContent) + 1;
-            if (Number(killCount.textContent) === 10) {
-                window.alert('Победа!');
-                killCount.textContent = 0
-                missCount.textContent = 0
-            }
-        } else {
-            missCount.textContent = Number(missCount.textContent) + 1;
-            if (Number(missCount.textContent) === 5) {
-                window.alert('Вы проиграли!');
-                killCount.textContent = 0
-                missCount.textContent = 0
-            }
+    if (holeClass === 'hole hole_has-mole') {
+        killCount.textContent = Number(killCount.textContent) + 1;
+        if (Number(killCount.textContent) === 10) {
+            window.alert('Победа!');
+            killCount.textContent = 0
+            missCount.textContent = 0
+        }
+    } else {
+        missCount.textContent = Number(missCount.textContent) + 1;
+        if (Number(missCount.textContent) === 5) {
+            window.alert('Вы проиграли!');
+            killCount.textContent = 0
+            missCount.textContent = 0
         }
     }
+}
